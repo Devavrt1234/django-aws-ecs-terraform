40038890abcd1ebfdad8141fac8c346844c6da58
DEVAVRAT SINGH <95224039+Devavrt1234@users.noreply.github.com>
2024-11-05 11:08:22 +0530

    Update app.js
    Signed-off-by: DEVAVRAT SINGH <95224039+Devavrt1234@users.noreply.github.com>

--- app.js
+++ app.js
@@ -1,11 +1,1 @@
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 7000;
 
-app.get('/', (req, res) => {
-    res.send('Hello from Node.js on EKS!');
-});
-
-app.listen(PORT, () => {
-    console.log(`Server is running on the port ${PORT}`);
-});
